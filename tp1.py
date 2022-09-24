f6b34140e2aec9a9baadd32f5d05052aa59a5a6a
abdoul-aziz55 <abdoul-aziz.hassane-mahamane@depinfonancy.net>
2022-09-24 21:29:36 +0200

    publication des changements sur le fichiers python

--- tp1.py
+++ tp1.py
@@ -25,77 +25,64 @@ def test(model, data, target):
 
     return acc
 
-def train(model, data, target): #la training loop (iterate) 
-    optim = torch.optim.Adam(model.parameters(), lr=1.0) #ADAM variante de SGD
+def train(model, data, target, nEpoch, learningRate): #la training loop (iterate) 
+    optim = torch.optim.Adam(model.parameters(), lr=learningRate) #ADAM variante de SGD
     criterion = nn.CrossEntropyLoss()
 
     x=torch.FloatTensor(data) #convertir en tenseur 
     y=torch.LongTensor(target)
     
-    L_acc=[]
-    ep=[]
-    L_acc2=[]
-    idx = np.arange(100)
-    for epoch in range(100): 
-        
+    accVector=[]
+    lossVector=[]
+    epochVector=[x for x in range(nEpoch)]
+    idx = np.arange(nEpoch)
+    print("Loss Accuracy")
+    for epoch in range(nEpoch): 
         np.random.shuffle(idx)
         tx=x[idx]
-        ty=y[idx]#torch.tensor(y[idx])
-        optim.zero_grad() #mettre a zero tt les gradients 
+        ty=y[idx]
+        optim.zero_grad() #mettre a zero tous les gradients 
         haty = model(tx) #appeler forward on a les logits  
         loss = criterion(haty,ty) #on complète le graphe par la loss
-        acc = test(model, tx ,ty) #data, target)
+        acc = test(model, tx ,ty) #data, target
         print(str(loss.item())+" "+str(acc))
-        L_acc2.append(loss.item())
+        lossVector.append(loss.item())
+        accVector.append(acc)
         loss.backward() #la backpropagation 
-        optim.step() #derniere etape modification des théta 
-        #je shuffle ici
+        optim.step() #derniere etape modification des théta
         
-        
-        L_acc.append(acc)
-        #L_acc2.append(str(loss.item()))
-        ep.append(epoch)
-    plt.plot(ep, L_acc,L_acc2)
-    #plt.plot(ep, L_acc2, 'r', label='loss')
+    #traçage des graphes
+    plt.plot(epochVector, accVector, label="dev acc")
+    plt.plot(epochVector, lossVector, label="train acc")
+    plt.legend(loc="upper right")
     plt.xlabel('Epochs')
-    plt.title('with lr=1.0')
+    plt.title("Expérience réalisée avec un taux d'apprentissage de "+str(learningRate))
     plt.show()
 
-def genData(n,nsamps): #nins, nsamps): #juste generation des données selon deux gaussiènes 
-    # prior0 = 0.7
-    # mean0  = 0.3
-    # var0   = 0.1
-    # mean1  = 0.8
-    # var1   = 0.01
-
+def genData(n, nsamps):  #juste generation des données selon deux gaussiènes 
     n0 = int(nsamps*0.8) #*prior0)
-    # x0=var0 * np.random.randn(n0,nins) + mean0
-    # x1=var1 * np.random.randn(nsamps-n0,nins) + mean1
-    # x = np.concatenate((x0,x1), axis=0)
-    # y = np.ones((nsamps,),dtype='int64')
-    # y[:n0] = 0
-    x =  np.random.uniform(size=(n,n)) #np.random.normal(3, 2.5, size=(n, n))
+    x =  np.random.uniform(size=(n,n))
     y= np.random.uniform(size=(n,1))
-   # e = np.random.normal(0.5, 0.8, size=x.shape)
-    f = (x+y)**1/2 + np.log(x**2 + np.abs(y)) #np.square(x) + np.exp(y)  + 1  #+ e 
+  
+    f = (x+y)**1/2 + np.log(x**2 + np.abs(y)) #definition de la fonction f
     y2 = np.ones((nsamps,),dtype='int64')
     y2[:n0] = 0 # optional
-    print(f.shape)
-    print(y2.shape)
-    return f,y2
-    #return x,y
+
+    return f, y2
+
 
 def toytest(): #expérience
-    print('start')
+    print("Début de l'expérience...")
     model = Net(100,10)
     x,y=genData(100, 500)
-    print(genData(100, 500))
-  
-    train(model,x,y)
+    nEpoch = 100
+    lr=0.001
+    print("Données pour l'expérience:\nx y")
+    print(str(x)+" "+str(y))
+
+    train(model, x, y, nEpoch, lr)
+
+    print("Fin de l'expérience.")
 
 if __name__ == "__main__":
     toytest()
-
-    #Run it and plot the curves for the loss and for the accuracy
-    #Adapt the code to create a test corpus with the same method as for the training corpus and evaluate your model
-
