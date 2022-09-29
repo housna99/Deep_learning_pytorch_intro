229259b49a5dcf0b5296723068723a07d5edaf64
abdoul-aziz55 <abdoul-aziz.hassane-mahamane@depinfonancy.net>
2022-09-29 19:46:13 +0200

    ise à jour du code, du README et des figures

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Deep_learning_pytorch_intro
 
-# Explication détaillée du code :
+## Explication détaillée du code :
 
-## Première partie: Definition de la classe Net
+### Première partie: Definition de la classe Net
 
 On commence par définir notre classe Net du module NeuralNet de Pytorch, qui représente le réseau de neuronnes.
 
@@ -14,7 +14,7 @@ Pour que cette classe soit bien definie, vu qu'on veut faire un model de feed-fo
     - La fonction _forward()_ qui décrit comment l'information se propage dans le réseau de neuronnes. Après la première couche, on applique
     une fonction d'activation linéaire _tanh()_ (c'est une fonction principalement utilisée pour la classification entre deux classes.)
 
-## Deuxième partie: Entrainement et évaluation du modèle
+### Deuxième partie: Entrainement et évaluation du modèle
 
 Les fonctions _train()_ et _test()_ sont deux fonctions utilisées respectivement pour entrainer et évaluer le modèle.
 
@@ -35,56 +35,69 @@ Les fonctions _train()_ et _test()_ sont deux fonctions utilisées respectivemen
     precision est obtenue par évaluation du modèle sur les données de test. Enfin on fait de l'étape de retropropagation pour mettre à jour
     les poids associés à chaque donnée.
 
-## Troisième partie: Génération des données
+### Troisième partie: Génération des données
 
 La fonction _gendata()_ a comme objectif la génération des données. Ici on choisit comme entrée _f(x,y)=log(x+y)_ et comme cible y2 qui est un vecteur de 0 et de 1 (2 classes supposées). Les paramètres x et y de la fonction f sont générés de façon uniforme et aléatoire.
 La fonction _gendata()_ renvoie alors f appliquée à x et y, et y2
 
-## Quatrième partie: Expérience
+### Quatrième partie: Expériences
 
 Dans le main, on fait appel à _toytest()_. Dans cette fonction, on réalise les différentes expériences. On définit les
 hyperparamètres du modèle et on fait l'entrainement du modèle
 
-# Résultats
+
+
+
+### Influence du taux d'apprentissage
+
+On a réalisé les 4 expériences suivantes pour essayer de voir le comportement du modèle avec des taux d'apprentissage différents :
 
 ![image](Experience_1.png)
 ![image](Experience_2.png)
 ![image](Experience_3.png)
 ![image](Experience_4.png)
-![image](Runtime&NbEpoch.png)
-
-# Analyse
 
 Dans les 4 expériences réalisées, le modèle de feed-forward entrainé arrive toujours, ou presque à apprendre notre fonction. En effet, la
 précision du modèle croit toujours et arrive à 1 lors des 3 expériences, sauf pour un taux d'apprentissage de 0.001 où on arrive très peu de
 fois à 0.99. Cependant, en repetant l'expérience avec le taux d'apprentissage constant égale à 0.001, on arrive à une precision de 100%.
 On a choisit de garder les mêmes données entre les expériences.
+
 On s'est amusés à chronométrer le temps que prend notre modèle pour apprendre la fonction f à chaque expérience, et les temps sont variables.
 On a constaté qu'entre la 1ère (lr=0.001) et la 2ème expérience (lr=0.01), ce temps a diminué mais après ne fait qu'augmenter (on a répété
 l'expérience plusieurs fois et c'est quasiment toujours ainsi que cela se présente).
 Aussi, on a mesuré le nombre d'epochs que necessite le modèle pour atteindre le maximum de précision. Celui décroit généralement, mais il
 augmente quasiment toujours quand on passe à un taux d'apprentissage de 1.0.
+
+Voici les résultats obtenus à travers ces mesures:
+
+![image](Runtime&NbEpoch.png)
+
 Une remarque intéréssante est que plus le taux d'apprentissage est grand, moins la précision augmente rapidement entre les epochs, sauf
 quelques fois avec un taux d'apprentissage de 1.0 où on peut avoir quelques perturbations.
 
-## optimizer ADAM vs SGD
+### optimiseur ADAM vs SGD
 
-L'optimizer a piqué notre attention, du coup nous avons décidé de comparer entre l'optimizer ADAM et celui du SGD.
-Les résultats montrent que l'optimizer d'ADAM marche en général mieux que celui du SGD.
-Pour un learning rate de 0.001, on trouve les résultats suivants:
+On a aussi réalisé une comparaison entre l'optimiseur ADAM et le SGD pour essayer de voir l'effet que cela aurait.
 
-    - Avec SGD, le modèle nécessite plus d'itérations pour converger.
-    - Avec ADAM, le modèle converge.
+On a fait une expérience avec les deux optimiseurs pour un learning rate de *0.001*, on trouve les résultats suivants:
 
-![image](Figure_1_SGD.png)
-![image](Figure_1_ADAM.png)
+![image](Comparaison_SGD_ADAM_1.png)
 
-Pour les autres valeurs du learning rate, on prend par exemple 0.01, on trouve les résultats suivants:
+Comme on peut le voir:
+    - Avec ADAM, le modèle converge en 20 itérations.
+    - Avec SGD, le modèle nécessite plus de 100 itérations pour espérer converger.
+    
+Pour confirmer l'écart de performance entre les deux optimiseurs appliqués à  notre modèle, on a refait la même expérience mais en
+prenant cette fois un taux d'apprentissage de *0.01*, on trouve les résultats suivants:
 
-    - Avec SGD, le modèle converge, mais il prend du temps et la loss function ne diminue pas autant que quand on       utilise l'optimizer d'ADAM.
-    - Avec ADAM, le modèle converge plus rapidement.
+![image](Comparaison_SGD_ADAM_2.png)
 
-![image](Figure_2_SGD.png)
-![image](Figure_2_ADAM.png)
+Comme on peut le voir:
+    - Avec ADAM, le modèle converge en 5 itérations.
+    - Avec SGD, le modèle ne converge qu'au bout de 53 epochs, donc beaucoup plus lentement qu'avec ADAM et la fonction de perte ne diminue
+    pas autant qu'avec ADAM.
+    
 
-On peut dire alors que le SGD peut aboutir à des meilleurs résultats seulement si on arrive à bien choisir une valeur du learning rate. ADAM reste le meilleur choix, puisqu'il est plus rapide et nécessite pas la modifications des paramètres.
+On peut dire alors que, pour le modèle qu'on a pour cette étude, le SGD peut aboutir à des bons résultats si on arrive à bien choisir une 
+valeur du learning rate adaptée pour le modèle choisi.. ADAM reste le meilleur choix pour ce modèle puisqu'il converge plus rapidement et
+diminue mieux la fonction de perte.
--- tp1.py
+++ tp1.py
@@ -26,9 +26,7 @@ def test(model, data, target):
 
     return acc
 
-def train(model, data, target, nEpoch,optim,learningRate): #la training loop (iterate) 
-    #optim = torch.optim.Adam(model.parameters(), lr=learningRate) #ADAM variante de SGD
-    #optim=torch.optim.SGD(model.parameters(), lr=learningRate)
+def train(model, data, target, nEpoch, optim, learningRate): #la training loop (iterate) 
     criterion = nn.CrossEntropyLoss()
 
     x=torch.FloatTensor(data) #convertir en tenseur 
@@ -70,7 +68,7 @@ def train(model, data, target, nEpoch,optim,learningRate): #la training loop (it
     return epochVector, accVector, lossVector, nbIterBeforeMaxAcc
 
 
-def genData(n, nsamps):  #generation des données selon deux gaussiennes 
+def genData(n, nsamps):  #generation des données
     n0 = int(nsamps*0.8)
     x =  np.random.uniform(size=(n,n))
     y= np.random.uniform(size=(n,n))
@@ -82,91 +80,101 @@ def genData(n, nsamps):  #generation des données selon deux gaussiennes
     return f, y2
 
 
-def toytest(): #expérience
+def toytest(): #expériences
+    # étude de l'influence du taux d'apprentissage
     lrs = [0.001, 0.01, 0.1, 1.0]
     nEpoch = 100
     runTimes = []
     nbEpochBeforeMaxAcc = []
     nbOfExperiences = 4
-    choix_optimizer=input('choose type of Optimizer: 1)ADAM or 2)SGD ')
     x,y=genData(100, 500)
-    if(choix_optimizer=='1'):
-        for i in range(nbOfExperiences):
-            print(f"Début de l'expérience {i+1} avec optimizer ADAM")
-            model = Net(100,10)
-            optim = torch.optim.Adam(model.parameters(), lr=lrs[i])
+    
+    for i in range(nbOfExperiences):
+        print(f"Début de l'expérience {i+1} d'étude de l'influence du lr...")
+        model = Net(100,10)
+        optim = torch.optim.Adam(model.parameters(), lr=lrs[i])
 
+        print("Données pour l'expérience:")
+        print(f"x:   {x} \ny:   {y}")
+        start = time.time()
+        epochVector, accVector, lossVector, nbIter = train(model, x, y, nEpoch, optim, lrs[i])
+        end = time.time()
+        runTimes.append(end - start)
+        nbEpochBeforeMaxAcc.append(nbIter)
+        print("Temps d'apprentissage d'environ: %f s" % runTimes[-1])
+        print(f"Nombre d'epochs pour avoir une précision de 100%: {nbIter}")
+
+        #traçage des graphes
+        plt.plot(epochVector, accVector, label="Accuracy")
+        plt.plot(epochVector, lossVector, label="Loss")
+        plt.legend(loc="upper right")
+        plt.xlabel('Epochs', fontsize=9)
+        plt.title(f"Expérience réalisée avec un taux d'apprentissage de {lrs[i]}", fontsize= 11)
+        plt.show()
+        print(f"Fin de l'expérience {i+1} d'étude de l'influence du lr.\n")
+    plt.subplot(3, 1, 1)
+    plt.plot([1, 2, 3, 4], runTimes, label="Temps d'apprentissage")
+    plt.legend(loc="upper right", prop={'size': 9})
+
+    plt.ylabel("Temps d'apprentissage en s", fontsize=8)
+    plt.title("Évolution du temps d'apprentissage entre les expériences", fontsize=11)
+
+    plt.subplot(3, 1, 3)
+    plt.plot([1, 2, 3, 4], nbEpochBeforeMaxAcc, label=f"Nombre d'epochs avant {100}% de précision", color='r')
+    plt.legend(loc="upper right", fontsize= 9)
+    plt.xlabel("n° d'expérience", fontsize= 9)
+    plt.ylabel("Nombre d'epochs", fontsize= 8)
+    plt.title("Évolution du nombre d'epochs necessaire entre les expériences", fontsize= 11)
+    plt.show()
+
+    #---------------------------------------------------#
+    # Comparaison des optimiseurs Adam et SGD
+    lrs = [0.001, 0.01]
+    nEpoch = 100
+    nbOfExperiences = 2
+    nbOfOptimiseurs = 2
+    labelOptim = ["ADAM", "SGD"]
+    x,y=genData(100, 500) # generation de nouvelles données pour cette experience
+    for i in range(nbOfExperiences):
+        epochVectors, accVectors, lossVectors = [], [], []
+
+        for j in range(nbOfOptimiseurs):
+            print(f"Début de l'expérience {i+1} de comparaison des optimiseurs avec {labelOptim[j]}...")
+            model = Net(100,10)
+            if j == 0:
+                optim = torch.optim.Adam(model.parameters(), lr=lrs[i])
+            elif j == 1:
+                optim = torch.optim.SGD(model.parameters(), lr=lrs[i])
             print("Données pour l'expérience:")
             print(f"x:   {x} \ny:   {y}")
             start = time.time()
             epochVector, accVector, lossVector, nbIter = train(model, x, y, nEpoch,optim, lrs[i])
+            epochVectors.append(epochVector)
+            accVectors.append(accVector)
+            lossVectors.append(lossVector)
             end = time.time()
-            runTimes.append(end - start)
-            nbEpochBeforeMaxAcc.append(nbIter)
-            print("Temps d'apprentissage d'environ: %f s" % runTimes[-1])
-            print(f"Nombre d'epochs pour avoir une précision de 100%: {nbIter}")
 
-            #traçage des graphes
-            plt.plot(epochVector, accVector, label="Accuracy")
-            plt.plot(epochVector, lossVector, label="Loss")
-            plt.legend(loc="upper right")
-            plt.xlabel('Epochs', fontsize=9)
-            plt.title(f"Expérience réalisée avec un taux d'apprentissage de {lrs[i]}, avec optimizer : ADAM", fontsize= 11)
-            plt.show()
-            print(f"Fin de l'expérience {i+1}.\n")
+            print(f"Temps d'apprentissage d'environ avec l'optimiseur {labelOptim[j]} {end - start} s")
+            print(f"Nombre d'epochs pour avoir une précision de 100% avec l'optimiseur {labelOptim[j]} : {nbIter}")
+
+            print(f"Fin de l'expérience {i+1} de comparaison des optimiseurs avec {labelOptim[j]}.\n")
+
+        #traçage des graphes
         plt.subplot(3, 1, 1)
-        plt.plot([1, 2, 3, 4], runTimes, label="Temps d'apprentissage")
-        plt.legend(loc="upper right", prop={'size': 9})
-
-        plt.ylabel("Temps d'apprentissage en s", fontsize=8)
-        plt.title("Évolution du temps d'apprentissage entre les expériences", fontsize=11)
+        plt.plot(epochVectors[0], accVectors[0], label="Accuracy")
+        plt.plot(epochVectors[0], lossVectors[0], label="Loss")
+        plt.legend(loc="upper right")
+        plt.xlabel('Epochs', fontsize=9)
+        plt.title(f"Expérience réalisée avec {labelOptim[0]} et un taux d'apprentissage de {lrs[i]}", fontsize= 11)
 
         plt.subplot(3, 1, 3)
-        plt.plot([1, 2, 3, 4], nbEpochBeforeMaxAcc, label=f"Nombre d'epochs avant {100}% de précision", color='r')
-        plt.legend(loc="upper right", fontsize= 9)
-        plt.xlabel("n° d'expérience", fontsize= 9)
-        plt.ylabel("Nombre d'epochs", fontsize= 8)
-        plt.title("Évolution du nombre d'epochs necessaire entre les expériences", fontsize= 11)
+        plt.plot(epochVectors[1], accVectors[1], label="Accuracy")
+        plt.plot(epochVectors[1], lossVectors[1], label="Loss")
+        plt.legend(loc="upper right")
+        plt.xlabel('Epochs', fontsize=9)
+        plt.title(f"Expérience réalisée avec {labelOptim[1]} et un taux d'apprentissage de {lrs[i]}", fontsize= 11)
         plt.show()
-    elif(choix_optimizer=='2'):
-        for i in range(nbOfExperiences):
-            print(f"Début de l'expérience {i+1} avec optimizer SGD")
-            model = Net(100,10)
-            optim = torch.optim.SGD(model.parameters(), lr=lrs[i])
+        
 
-            print("Données pour l'expérience:")
-            print(f"x:   {x} \ny:   {y}")
-            start = time.time()
-            epochVector, accVector, lossVector, nbIter = train(model, x, y, nEpoch,optim, lrs[i])
-            end = time.time()
-            runTimes.append(end - start)
-            nbEpochBeforeMaxAcc.append(nbIter)
-            print("Temps d'apprentissage d'environ: %f s" % runTimes[-1])
-            print(f"Nombre d'epochs pour avoir une précision de 100%: {nbIter}")
-
-            #traçage des graphes
-            plt.plot(epochVector, accVector, label="Accuracy")
-            plt.plot(epochVector, lossVector, label="Loss")
-            plt.legend(loc="upper right")
-            plt.xlabel('Epochs', fontsize=9)
-            plt.title(f"Expérience réalisée avec un taux d'apprentissage de {lrs[i]}, avec optimizer : SGD", fontsize= 11)
-            plt.show()
-            print(f"Fin de l'expérience {i+1}.\n")
-        plt.subplot(3, 1, 1)
-        plt.plot([1, 2, 3, 4], runTimes, label="Temps d'apprentissage")
-        plt.legend(loc="upper right", prop={'size': 9})
-
-        plt.ylabel("Temps d'apprentissage en s", fontsize=8)
-        plt.title("Évolution du temps d'apprentissage entre les expériences", fontsize=11)
-
-        plt.subplot(3, 1, 3)
-        plt.plot([1, 2, 3, 4], nbEpochBeforeMaxAcc, label=f"Nombre d'epochs avant {100}% de précision", color='r')
-        plt.legend(loc="upper right", fontsize= 9)
-        plt.xlabel("n° d'expérience", fontsize= 9)
-        plt.ylabel("Nombre d'epochs", fontsize= 8)
-        plt.title("Évolution du nombre d'epochs necessaire entre les expériences", fontsize= 11)
-        plt.show()
-    else:
-        print('choose 1 or 2')
 if __name__ == "__main__":
     toytest()
